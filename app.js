002f87b967d30b11fbcddb36a81e26feb02afb6a
sagar8789520414 <144613860+sagar8789520414@users.noreply.github.com>
2024-02-13 11:23:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
 const store = MongoStore.create({
-  mongoUrl: dbUrl,
+  mongoUrl: `${dbUrl}`,
   crypto: {
     secret: process.env.SECRET,
   },
